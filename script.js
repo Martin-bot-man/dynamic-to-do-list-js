ae0ef149ac6550f8980b37be13086869ebc0fc10
Martin Odongo <martinwere14@gmail.com>
2024-10-13 17:21:55 +0300

    added a class to the listitem func

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded',event =>{
     const addButton = document.getElementById('add-task-btn');
     const taskInput = document.getElementById('task-input');
     const taskList = document.getElementById('task-list');
-
+//add task function
     function addTask(){
 const taskText = taskInput.value.trim();
 
@@ -14,8 +14,12 @@ if(taskText===''){
 }
 const listItem = document.createElement('li');
 listItem.textContent= taskText;
+//add class to the list item
+listItem.classList.add('task-item')
+//remove button
 const removeButton = document.createElement('button');
 removeButton.textContent= "Remove";
+
 removeButton.className ="remove-btn";
     
 removeButton.onClick = function(){
@@ -24,13 +28,14 @@ listItem.remove();
 listItem.appendChild(removeButton);
 taskList.appendChild(listItem);
 taskInput.value=''
+//attach event listeners
 addButton.addEventListener("onClick",addTask);
 taskInput.addEventListener("keypress", function(event){
     if(event.key==="Enter"){
         addTask();
     }
 })
-}
+
     };
 function addTask(){
     document.addEventListener('DOMContentLoaded', addTask)
