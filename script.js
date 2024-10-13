5e01ea9c2fc8fab8e0c36c90e7ea2ea3e19e1e2e
Martin Odongo <martinwere14@gmail.com>
2024-10-13 17:28:16 +0300

    js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ listItem.appendChild(removeButton);
 taskList.appendChild(listItem);
 taskInput.value=''
 //attach event listeners
-addButton.addEventListener("Click",addTask);
+addButton.addEventListener("click",addTask);
 taskInput.addEventListener("keypress", function(event){
     if(event.key==="Enter"){
         addTask();
